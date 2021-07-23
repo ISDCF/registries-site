5e2227f15568e72de9d0ccef27108b1549ab969d
palemieux <palemieux@users.noreply.github.com>
2021-07-23 00:29:59 +0000

    deploy: 9215df83da3f1fa3a975585ce03e6a4f02c0fd93

---
+++ features.js
@@ -0,0 +1,50 @@
+/* Filter accross all collumns in table */
+
+$(document).ready(function(){
+  $("#search").on("input", function() {
+    var value = $(this).val().toLowerCase();
+    $("#searchTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+
+/* Clear filtering*/
+
+$(document).on('click', '.clear-filter', function(){       
+    $("#searchTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf('') > -1)
+    });
+    document.getElementById("search-form").reset();
+});
+
+/* DataTable options for sort headers*/
+
+$(document).ready(function() {
+    $('#sorttable').DataTable( {
+      "paging": false,
+      "info": false,
+      "searching": false,
+      fixedHeader: true,
+      "order": [[ 0, "asc" ]]
+    });
+} );
+
+/* "Back To Top" button functionality */
+
+$(document).ready(function() {
+$(window).scroll(function() {
+if ($(this).scrollTop() > 20) {
+$('#toTopBtn').fadeIn();
+} else {
+$('#toTopBtn').fadeOut();
+}
+});
+
+$('#toTopBtn').click(function() {
+$("html, body").animate({
+scrollTop: 0
+}, 1000);
+return false;
+});
+});
